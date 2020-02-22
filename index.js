126469960f3f3073d5ca1f2aad4106f440de6c8e
cejimenez0123 <christianjimenez0123@gmail.com>
2020-02-22 11:55:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+let leftNumbers = dodger.style.left.replace("px", "");
+let left = parseInt(leftNumbers, 10);
+
+document.addEventListener("keydown", function(e) {
+  moveDodgerLeft(e);
+});
+
+document.addEventListener("keydown", function(e) {
+  moveDodgerRight(e);
+});
+
+function moveDodgerLeft() {
+    debugger;
+//   if ((e.key == "ArrowLeft") && (left > 0 )){
+      dodger.style.left = `${left -= 1}px`;
+//   }
+}
+
+function moveDodgerRight() {
+//   if ((e.key == "ArrowRight") && (left < 360 )){
+      dodger.style.left = `${left += 1}px`;
+//   }
+}
